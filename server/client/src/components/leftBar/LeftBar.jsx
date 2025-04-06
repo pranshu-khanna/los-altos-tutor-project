3fc3c9e69cf1cfbadb867d2ea1138349cdee91d6
Pranshu K. '28 <pranshu.challenger@gmail.com>
2025-04-05 17:57:01 -0700

    Added LeftBar and NavBar
    Deleted the right bar; realized won't need it
    
    NavBar --- logo, functionality for dark mode, and profile picture
    
    LeftBar --- calendar, enrolled classes, find classes, settings, and log out

--- server/client/src/components/leftBar/LeftBar.jsx
+++ server/client/src/components/leftBar/LeftBar.jsx
@@ -1,21 +1,53 @@
 import "./leftBar.scss";
-import {useContext} from "react";
+import React, {useContext} from "react";
+import {Link} from "react-router-dom";
+import Calendar from "../../assets/6.png";
+import Class from "../../assets/class.png";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import SettingsIcon from '@mui/icons-material/Settings';
+import LogoutIcon from '@mui/icons-material/Logout';
 import {AuthContext} from "../../context/authContext";
 
 const LeftBar = () => {
   const {currentUser} = useContext(AuthContext);
 
   return (
-      <div className="leftBar">
-         <div className="container">
-          <div className="menu"></div>
-          <hr/>
-          <div className="menu"></div>
-          <hr />
-          <div className="menu"></div>
+    <div className="leftBar">
+      <div className="container">
+        <div className="menu">
+          <div className="item">
+            <Link to="/calendar" style={{ textDecoration: "none", display: "flex", alignItems: "center", gap: "15px" }}>
+              <img src={Calendar} alt="Calendar Icon" />
+              <span>My Calendar</span>
+            </Link>
+          </div>
+          <div className="item">
+            <Link to="/classes" style={{ textDecoration: "none", display: "flex", alignItems: "center", gap: "15px" }}>
+              <img src={Class} alt="Classes Icon" />
+              <span>My Classes</span>
+            </Link>
+          </div>
+          <div className="item">
+            <Link to="/search" style={{ textDecoration: "none", display: "flex", alignItems: "center", gap: "15px" }}>
+              <SearchOutlinedIcon />
+              <span>Find Classes</span>
+            </Link>
+          </div>
+        </div>
+        <div className="bottom-menu">
+        <div className="divider"/>
+          <div className="item">
+            <SettingsIcon />
+            <span>Settings</span>
+          </div>
+          <div className="item">
+            <LogoutIcon />
+            <span>Logout</span>
+          </div>
         </div>
       </div>
+    </div>
   );
-}
+};
 
 export default LeftBar;
--- server/client/src/components/navBar/NavBar.jsx
+++ server/client/src/components/navBar/NavBar.jsx
@@ -1,12 +1,7 @@
+import React from "react";
 import "./navBar.scss";
-import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
 import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
-import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
-import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
-import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import {Link} from "react-router-dom";
 import {useContext} from "react";
 import {DarkModeContext} from "../../context/darkModeContext";
@@ -19,20 +14,11 @@ const NavBar = () => {
         <div className="navBar">
           <div className="left">
             <Link to="/" style={{textDecoration: "none"}}>
-              <span>tutoring-platform</span>
+              <span>upskill-me</span>
             </Link>
-            <HomeOutlinedIcon/>
             {darkMode ? <WbSunnyOutlinedIcon onClick = {toggle}/> : <DarkModeOutlinedIcon onClick = {toggle}/>}
-            <GridViewOutlinedIcon/>
-            <div className="search">
-              <SearchOutlinedIcon/>
-              <input type="text" placeholder="Search..." />
-            </div>
           </div>
           <div className="right">
-            <PersonOutlinedIcon/>
-            <EmailOutlinedIcon/>
-            <NotificationsOutlinedIcon/>
             <div className="user">
               <img alt=""/>
               <span>John Doe</span>

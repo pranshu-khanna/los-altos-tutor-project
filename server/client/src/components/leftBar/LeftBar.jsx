229fdad3951bd828f64d7c44218260a3470dc14b
Pranshu K. '28 <pranshu.challenger@gmail.com>
2025-04-05 18:21:52 -0700

    Added assets & updated LeftBar
    updated LeftBar such that every item links to another page

--- server/client/src/components/leftBar/LeftBar.jsx
+++ server/client/src/components/leftBar/LeftBar.jsx
@@ -1,32 +1,63 @@
 import "./leftBar.scss";
-import React, {useContext} from "react";
-import {Link} from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
 import Calendar from "../../assets/calendar.png";
 import Class from "../../assets/class.png";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandCircleDown';
 import SettingsIcon from '@mui/icons-material/Settings';
 import LogoutIcon from '@mui/icons-material/Logout';
-import {AuthContext} from "../../context/authContext";
+import { AuthContext } from "../../context/authContext";
 
 const LeftBar = () => {
   const {currentUser} = useContext(AuthContext);
+  const [classesExpanded, setClassesExpanded] = useState(false);
+
+  const dummyClasses = [
+    "math",
+    "english",
+    "spanish",
+    "history",
+    "robotics",
+  ];
 
   return (
     <div className="leftBar">
       <div className="container">
         <div className="menu">
           <div className="item">
-            <Link to="/calendar" style={{ textDecoration: "none", display: "flex", alignItems: "center", gap: "15px" }}>
+            <Link to="/calendar" style={{textDecoration: "none", display: "flex", alignItems: "center", gap: "15px"}}>
               <img src={Calendar} alt="Calendar Icon" />
               <span>My Calendar</span>
             </Link>
           </div>
-          <div className="item">
-            <Link to="/classes" style={{ textDecoration: "none", display: "flex", alignItems: "center", gap: "15px" }}>
+          <div className="item" onClick={() => setClassesExpanded(!classesExpanded)} style={{cursor: "pointer"}}>
+            <div style={{display: "flex", alignItems: "center", gap: "15px"}}>
               <img src={Class} alt="Classes Icon" />
               <span>My Classes</span>
-            </Link>
+              {classesExpanded ? <ExpandLessIcon fontSize="small" /> : <ExpandMoreIcon fontSize="small" />}
+            </div>
           </div>
+
+          {classesExpanded && (
+            <div className="class-list">
+              {dummyClasses.map((cls) => (
+                <Link
+                  to={`/${cls.toLowerCase()}`}
+                  key={cls}
+                  style={{
+                    textDecoration: "none",
+                    marginLeft: "45px",
+                    color: "inherit",
+                  }}
+                >
+                  <div className="subclass-item">{cls.charAt(0).toUpperCase() + cls.slice(1)}</div>
+                </Link>
+              ))}
+            </div>
+          )}
+
           <div className="item">
             <Link to="/search" style={{ textDecoration: "none", display: "flex", alignItems: "center", gap: "15px" }}>
               <SearchOutlinedIcon />
@@ -34,15 +65,21 @@ const LeftBar = () => {
             </Link>
           </div>
         </div>
+
+        <div className="divider" />
+
         <div className="bottom-menu">
-        <div className="divider"/>
           <div className="item">
-            <SettingsIcon />
-            <span>Settings</span>
+            <Link to ="/settings" style={{textDecoration: "none", display: "flex", alignItems: "center", gap: "15px"}}>
+              <SettingsIcon />
+              <span>Settings</span>
+            </Link>
           </div>
           <div className="item">
-            <LogoutIcon />
-            <span>Logout</span>
+            <Link to ="/logout" style={{textDecoration: "none", display: "flex", alignItems: "center", gap: "15px"}}>
+              <LogoutIcon />
+              <span>Logout</span>
+            </Link>
           </div>
         </div>
       </div>
